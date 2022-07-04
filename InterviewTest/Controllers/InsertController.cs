4628796fb118b4fa97d7521830cd705a867af9d5
3370534 <3370534@MXCHILAP1910>
2022-07-04 12:23:09 -0600

    Add project files.

---
+++ InterviewTest/Controllers/InsertController.cs
@@ -0,0 +1,103 @@
+﻿using InterviewTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterviewTest.Controllers
+{
+    /// <summary>
+    /// Controller for the views that insert data to the database
+    /// </summary>
+    public class InsertController : Controller
+    {
+        /// <summary>
+        /// Open the View to Insert a building
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertBuilding()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Receive the new building object and if it's valid adds it to the data base 
+        /// </summary>
+        /// <param name="building"></param>
+        /// <returns>the view</returns>
+        [HttpPost]
+        public ActionResult InsertBuilding([Bind(Include = "PKBuilding, Building1")]Building building)
+        {
+            if (ModelState.IsValid)
+            {
+                MaterialsEntities dataBaseEntity = new MaterialsEntities();
+                dataBaseEntity.Buildings.Add(building);
+                dataBaseEntity.SaveChanges();
+                ViewBag.message = "Building added successfully";
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// Open the View to Insert a customer
+        /// </summary>
+        /// <returns>the view</returns>
+        public ActionResult InsertCustomer()
+        {
+            MaterialsEntities dataBaseEntity = new MaterialsEntities();
+            ViewBag.Buildings = dataBaseEntity.Buildings.ToList();
+            return View();
+        }
+
+        /// <summary>
+        /// Receive the new customer object and if it's valid adds it to the data base 
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns>the view</returns>
+        [HttpPost]
+        public ActionResult InsertCustomer([Bind(Include = "PKCustomer, Customer1, Prefix, FKBuilding")] Customer customer)
+        {
+            MaterialsEntities dataBaseEntity = new MaterialsEntities();
+            ViewBag.Buildings = dataBaseEntity.Buildings.ToList();
+
+            if (ModelState.IsValid)
+            {
+                dataBaseEntity.Customers.Add(customer);
+                dataBaseEntity.SaveChanges();
+                ViewBag.message = "Customer added successfully";
+            }
+            return View();
+        }
+
+        /// <summary>
+        /// Open the View to Insert a part number
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertPartNumber()
+        {
+            MaterialsEntities dataBaseEntity = new MaterialsEntities();
+            ViewBag.Customers = dataBaseEntity.Customers.ToList();
+            return View();
+        }
+
+        /// <summary>
+        /// Receive the new part number object and if it's valid adds it to the data base 
+        /// </summary>
+        /// <param name="partNumber"></param>
+        /// <returns>view</returns>
+        [HttpPost]
+        public ActionResult InsertPartNumber([Bind(Include = "PKPartNumber, PartNumber1, FKCustomer, Available")] PartNumber partNumber)
+        {
+            MaterialsEntities dataBaseEntity = new MaterialsEntities();
+            ViewBag.Customers = dataBaseEntity.Customers.ToList();
+            if (ModelState.IsValid)
+            {
+                dataBaseEntity.PartNumbers.Add(partNumber);
+                dataBaseEntity.SaveChanges();
+                ViewBag.message = "Part Number added successfully";
+            }
+            return View();
+        }
+    }
+}
---
+++ InterviewTest/Controllers/QueryController.cs
@@ -0,0 +1,90 @@
+﻿using InterviewTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InterviewTest.Controllers
+{
+    public class QueryController : Controller
+    {
+        /// <summary>
+        /// Get the data to show in the view, sort it and send it to the view of to export to CSV
+        /// </summary>
+        /// <param name="search"></param>
+        /// <param name="available"></param>
+        /// <param name="notAvailable"></param>
+        /// <param name="sortColumn"></param>
+        /// <param name="iconClass"></param>
+        /// <param name="exportCSV"></param>
+        /// <returns>the view</returns>
+        public ActionResult ShowData(string search = "", bool available = false, bool notAvailable = false, string sortColumn = "PartNumber", string iconClass = "fa-sort-asc", bool exportCSV = false)
+        {
+            MaterialsEntities dataBaseEntity = new MaterialsEntities();
+            List<GetGridViewData_Result> dataList = dataBaseEntity.GetGridViewData().ToList();
+            List<GetGridViewData_Result> dataSend = new List<GetGridViewData_Result>();
+            ViewBag.search = search;
+            ViewBag.available = available;
+            ViewBag.notAvailable = notAvailable;
+            ViewBag.sortColumn = sortColumn;
+            ViewBag.iconClass = iconClass;
+
+            foreach (var data in dataList)
+            {
+                if (data.PartNumber.ToLower().Contains(search.ToLower()) || data.Customer.ToLower().Contains(search.ToLower()))
+                    if((available == true && data.Available == true) || (notAvailable == true && data.Available == false))
+                        dataSend.Add(data);
+            }
+            if (ViewBag.sortColumn == "PartNumber")
+            {
+                if (ViewBag.iconClass == "fa-sort-asc")
+                    dataSend = dataSend.OrderBy(temp => temp.PartNumber).ToList();
+                else
+                    dataSend = dataSend.OrderByDescending(temp => temp.PartNumber).ToList();
+            }
+            else if (ViewBag.sortColumn == "Customer")
+            {
+                if (ViewBag.iconClass == "fa-sort-asc")
+                    dataSend = dataSend.OrderBy(temp => temp.Customer).ToList();
+                else
+                    dataSend = dataSend.OrderByDescending(temp => temp.Customer).ToList();
+            }
+            else if (ViewBag.sortColumn == "Building")
+            {
+                if (ViewBag.iconClass == "fa-sort-asc")
+                    dataSend = dataSend.OrderBy(temp => temp.Building).ToList();
+                else
+                    dataSend = dataSend.OrderByDescending(temp => temp.Building).ToList();
+            }
+            else if (ViewBag.sortColumn == "Available")
+            {
+                if (ViewBag.iconClass == "fa-sort-asc")
+                    dataSend = dataSend.OrderBy(temp => temp.Available).ToList();
+                else
+                    dataSend = dataSend.OrderByDescending(temp => temp.Available).ToList();
+            }
+            if (exportCSV)
+                return ExportToCSV(dataSend);
+            else
+                return View(dataSend);
+        }
+
+        /// <summary>
+        /// Get the data to export it to CSV
+        /// </summary>
+        /// <param name="getGridViewData_Results"></param>
+        /// <returns>the CSV file</returns>
+        public ActionResult ExportToCSV(List<GetGridViewData_Result> getGridViewData_Results)
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("PartNumber,Customer,Building,Available");
+            foreach (var d in getGridViewData_Results)
+            {
+                builder.AppendLine($"{d.PartNumber},{d.Customer},{d.Building},{d.Available}");
+            }
+            return File(Encoding.UTF8.GetBytes(builder.ToString()), "text/csv", "Data.csv");
+        }
+    }
+}
